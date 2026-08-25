d047702c63ff1396578dc31a39846501cf3b60b8
Jatin Kateja <138096077+dev-jatin07@users.noreply.github.com>
2026-08-25 15:22:20 +0530

    Update 141_linked_list_cycle.js - LeetCode Problem 141

--- 141_linked_list_cycle.js
+++ 141_linked_list_cycle.js
@@ -13,11 +13,10 @@
 var hasCycle = function(head) {
     let slow = head;
     let fast = head;
-// jatin 
     while (fast != null && fast.next != null){
         slow = slow.next;
         fast = fast.next.next;
-        if (slow = fast){
+        if (slow === fast){
             return true
         }
     }
