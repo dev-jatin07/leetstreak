0cd427ca3dd06471952ef56fab648837c1600129
Jatin Kateja <138096077+dev-jatin07@users.noreply.github.com>
2026-09-11 02:48:14 +0530

    Update 724_find_pivot_index.js - LeetCode Problem 724

--- 724_find_pivot_index.js
+++ 724_find_pivot_index.js
@@ -5,22 +5,17 @@
 var pivotIndex = function(nums) {
     let left = 0;
     let sum = 0;
-
-    for (let num of nums) {
+    for (let num of nums){
         sum += num;
     }
-
-    for (let i = 0; i < nums.length; i++) {
-        if (i > 0) {
-            left += nums[i - 1];
+    for (let i = 0 ; i <nums.length ; i++){
+        if (i > 0){
+            left += nums[i-1];
         }
-
-        let right = sum - nums[i] - left;
-
-        if (left === right) {
-            return i;
+        let right = sum - nums[i] -left;
+        if (left === right){
+            return i
         }
     }
-
     return -1;
 };
