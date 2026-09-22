46ab6075f9eb0247068d1fd0e8533f952fe61a26
Jatin Kateja <138096077+dev-jatin07@users.noreply.github.com>
2026-09-22 23:52:13 +0530

    Add 15_3sum.js - LeetCode Problem 15

---
+++ 15_3sum.js
@@ -0,0 +1,44 @@
+var threeSum = function(nums) {
+    let ans = [];
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+
+        // Duplicate first element skip
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+
+            let sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                ans.push([nums[i], nums[left], nums[right]]);
+
+                // Duplicate values skip
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left++;
+                }
+
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right--;
+                }
+
+                left++;
+                right--;
+
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return ans;
+};
